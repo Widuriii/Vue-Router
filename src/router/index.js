d5a75eb26d96f236d6d74682f17cf23d3a29396f
Widuriii <widuriwii921@gmail.com>
2025-05-25 15:37:53 +0700

    index.js

---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Dashboard from '../views/Dashboard.vue';
+import Shop from '../views/Shop.vue';
+import Tutorial from '../views/Tutorial.vue';
+import AboutUs from '../views/AboutUs.vue';
+import { Component } from "react";
+
+const routes = [
+    {
+        path:'/',
+        redirect: '/dashboard',
+    },
+    {
+        path:'/dashboard',
+        name:'Dashboard',
+        component: Dashboard,
+    },
+    {
+        path:'/shop',
+        name:'Shop',
+        component: Shop,
+    },
+    {
+        path:'/tutorial',
+        name:'Tutorial',
+        component: Tutorial,
+    },
+    {
+        path:'/about',
+        name:'AboutUs',
+        component: AboutUs,
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
